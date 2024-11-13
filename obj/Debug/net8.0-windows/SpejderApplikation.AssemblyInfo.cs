9d499ccbc6ca18a9e49f4e50cdb4c008a85b2fa7
nicolai jensen <nbje63269@edu.ucl.dk>
2024-11-13 08:59:19 +0100

    måske nu

--- obj/Debug/net8.0-windows/SpejderApplikation.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/SpejderApplikation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpejderApplikation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9838b66bac523845d97c0a9f538a64fdfebd2548")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb421b6277efc0e389bd15b57188f0404bc75ff6")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpejderApplikation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpejderApplikation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
