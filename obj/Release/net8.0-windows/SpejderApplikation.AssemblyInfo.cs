5105d47d59556f8579ae1ac15fee1af929ccb229
nicolai jensen <nbje63269@edu.ucl.dk>
2024-11-13 08:47:21 +0100

    Update user paths and add auto-generated WPF files
    Updated file paths and user-specific directories from `pibm9` to `nbjer` in various configuration and cache files. Updated `AssemblyInformationalVersionAttribute` in `SpejderApplikation.AssemblyInfo.cs`. Added auto-generated code files for the WPF application "SpejderApplikation", including `App.g.cs` and `MainWindow.g.cs`. Updated `.editorconfig` with new build properties and added `SpejderApplikation.GlobalUsings.g.cs` for global using directives. Updated metadata and references in cache and lref files.

--- obj/Release/net8.0-windows/SpejderApplikation.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/SpejderApplikation.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpejderApplikation")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4075e78698e838d94d7f2b7a0afea2a8943df6b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpejderApplikation")]
