c67709323fc9b5ab92a3301159fbcbce74d689f8
nicolai jensen <nbje63269@edu.ucl.dk>
2024-11-28 11:31:16 +0100

    hyperlink virker, og koblet til kort nicolai database

--- View/Login.xaml.cs
+++ View/Login.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace SpejderApplikation.View
@@ -42,6 +44,12 @@ namespace SpejderApplikation.View
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
+            e.Handled = true;
         }
     }
 }
