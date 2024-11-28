ffe6796952e9910d9b7c4b58fbffda9ddf64646e
PeterIlsted <pibe55630@edu.ucl.dk>
2024-11-28 21:51:42 +0100

    Oprettet unit tests, kæmper stadigvæk med Stored Procedures

---
+++ Test/Insert data i tabeller.sql
@@ -0,0 +1,26 @@
+INSERT INTO Meeting(Date, Start, Stop)
+VALUES ('2024-11-27', '17:00:00', '19:00:00')
+
+INSERT INTO Meeting(Date, Start, Stop)
+VALUES ('2024-12-4', '17:00:00', '19:00:00')
+
+INSERT INTO Meeting(Date, Start, Stop)
+VALUES ('2024-12-11', '17:00:00', '19:00:00')
+
+INSERT INTO Badge(BadgeName, Description, Link)
+VALUES('Navigatør', 'Moral og etik', 'https://kfumspejderne.dk/maerke/navigatoer/')
+
+INSERT INTO Badge(BadgeName, Description, Link)
+VALUES('Knivbevis', 'Færdighed', 'https://kfumspejderne.dk/maerke/knivbevis/')
+
+INSERT INTO Badge(BadgeName, Description, Link)
+VALUES('Superhelte', 'Motoriske bevægelser', 'https://kfumspejderne.dk/maerke/superhelte/')
+
+INSERT INTO Activity(ActivityDescription, Preparation, Notes, Activity)
+VALUES ('Snakke om trafikregler i forskellige kontekster', 'Berit forbereder', NULL, 'Snak om trafikken')
+
+INSERT INTO Activity(ActivityDescription, Preparation, Notes, Activity)
+VALUES ('Opsumere regler for høj dolkføring og snitte træmænd', 'Husk at finde knive frem', 'John har ansvaret', 'Arbejde med knivbevis')
+
+INSERT INTO Activity(ActivityDescription, Preparation, Notes, Activity)
+VALUES ('Hvilke specielle evner har bæverne hver for sig?', 'Husk at forberede lidt konkurencer', 'Mia forbereder', 'Finde superkræfter')
